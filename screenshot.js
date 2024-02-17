d021069074fba40f3de876569f0f48b734b71860
Suman Shrestha <summonshr@gmail.com>
2024-02-17 09:56:08 +0545

    Add SQLite database functionality for storing URLs and elements

--- screenshot.js
+++ screenshot.js
@@ -1,6 +1,24 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const { trimEnd } = require('lodash');
+const { Statement } = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database');
+
+db.serialize(() => {
+    db.run("Drop table if exists urls");
+    db.run("CREATE TABLE IF NOT EXISTS urls (url TEXT, element varchar2(255),  UNIQUE(url, element))");
+
+    // const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    // for (let i = 0; i < 10; i++) {
+    //     stmt.run("Ipsum " + i);
+    // }
+    // stmt.finalize();
+
+    // db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+    //     console.log(row.id + ": " + row.info);
+    // });
+});
 
 function storagePath(path) {
     const directory = __dirname + '/screenshots/';
@@ -13,10 +31,12 @@ const browser = puppeteer.launch({
     defaultViewport: { width: 1920, height: 1080 }
 });
 
-async function takeScreenshot(query, imageName) {
+async function takeScreenshot(query) {
+    const page = await (await browser).newPage();
+
     try {
-        const page = await (await browser).newPage();
-        await page.goto(query.url.startsWith('http') ? query.url : 'https://' + query.url);
+        await page.goto(query.url.startsWith('http') ? query.url : 'https://' + query.url, { waitUntil: 'domcontentloaded' });
+        await page.waitForSelector(query.element, { visible: true })
         const element = await page.$(query.element);
         const boundingBox = await element.boundingBox();
         const screenshot = await page.screenshot({
@@ -28,6 +48,8 @@ async function takeScreenshot(query, imageName) {
             }
         });
 
+        await page.close();
+
         const folder = storagePath(query.element + '/' + query.url);
 
         if (!fs.existsSync(folder)) {
@@ -35,9 +57,8 @@ async function takeScreenshot(query, imageName) {
         }
 
         fs.writeFileSync(folder + '/screenshot.png', screenshot);
-        page.close();
-        return screenshot;
     } catch (error) {
+        await page.close();
         console.log(error)
     }
 }
@@ -46,14 +67,39 @@ const fastify = require('fastify')({
     logger: true
 })
 
-let screenshots = [];
-
-
 const rerun = async function () {
     try {
-        if (screenshots.length > 0) {
-            await takeScreenshot(screenshots.pop());
-        }
+        db.serialize(async () => {
+            try {
+                const rows = await new Promise((resolve, reject) => {
+                    db.all("SELECT * FROM urls", (err, rows) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve(rows);
+                        }
+                    });
+                });
+
+                for await (const row of rows) {
+                    await new Promise((resolve, reject) => {
+                        const deleteStmt = db.prepare("DELETE FROM urls WHERE url = ? AND element = ?");
+                        deleteStmt.run(row.url, row.element, (err) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                console.log(`Deleted url: ${row.url} and element: ${row.element}`);
+                                deleteStmt.finalize();
+                                resolve();
+                            }
+                        });
+                    });
+                    await takeScreenshot(row);
+                }
+            } catch (error) {
+                console.error(error.message);
+            }
+        });
     } catch (error) {
 
     }
@@ -65,7 +111,19 @@ setTimeout(rerun, 10000);
 // Declare a route
 fastify.get('/screenshots/:element/*', async function (request, reply) {
     let url = request.params['*'];
-    screenshots.push({ url: trimEnd(url, 'screenshot.png'), element: request.params.element })
+
+    try {
+        const stmt = db.prepare("INSERT OR IGNORE INTO urls VALUES (?, ?)");
+        stmt.run(trimEnd(url, 'screenshot.png'), request.params.element);
+        stmt.run(trimEnd(url, 'screenshot.png'), 'html');
+        stmt.run(trimEnd(url, 'screenshot.png'), 'body');
+        stmt.run(trimEnd(url, 'screenshot.png'), 'article');
+        stmt.run(trimEnd(url, 'screenshot.png'), 'pre');
+        stmt.run(trimEnd(url, 'screenshot.png'), 'a');
+        stmt.finalize();
+    } catch (error) {
+        console.log(error)
+    }
     reply
         .send({ 'message': "Added to queue" });
 })
