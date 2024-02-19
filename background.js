cd579307489dc59851e89fce05e342127480939f
Suman Shrestha <summonshr@gmail.com>
2024-02-19 20:45:58 +0545

    Should fix

--- background.js
+++ background.js
@@ -4,41 +4,52 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database');
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS urls (url TEXT, element varchar2(255),  UNIQUE(url, element))");
+    db.run("CREATE TABLE IF NOT EXISTS urls (url TEXT, element TEXT,  UNIQUE(url, element))");
 });
 
-function storagePath(path) {
+async function storagePath(path) {
     const directory = __dirname + '/screenshots/';
     return directory + path;
 }
 
-const browser = puppeteer.launch({
-    timeout: 60000,
-    headless: process.argv.includes('--prod') ? "new" : false,
-    args: ['--window-size=1920,1080', '--disable-notifications', '--no-sandbox'],
-    defaultViewport: { width: 1920, height: 1080 }
-});
+const launchBrowser = async () => {
+    const browser = await puppeteer.launch({
+        timeout: 60000,
+        headless: process.argv.includes('--prod') ? true : false,
+        args: ['--window-size=1920,1080', '--disable-notifications', '--no-sandbox'],
+        defaultViewport: { width: 1920, height: 1080 }
+    });
+    return browser;
+};
 
-async function takeScreenshot(query) {
-    const folder = storagePath(query.element + '/' + query.url);
-    const page = await (await browser).newPage();
+const takeScreenshot = async (query, browser) => {
+    const folder = await storagePath(query.element + '/' + query.url);
+    const screenshotPath = folder + '/screenshot.png';
+
+    // Check if the screenshot already exists
+    if (fs.existsSync(screenshotPath)) {
+        console.log(`Screenshot already exists for: ${folder}`);
+        return;
+    }
+
+    const page = await browser.newPage();
 
     try {
         await page.goto(query.url.startsWith('http') ? query.url : 'https://' + query.url, { waitUntil: 'domcontentloaded' });
         try {
             await page.waitForSelector(query.element, { visible: true });
         } catch (error) {
-	    console.log(error)
+            console.log(error);
             await page.close();
-	    return;
+            return;
         }
         const element = await page.$(query.element);
 
         await page.evaluate((element) => {
-            element.style['min-width'] = '800px';
-            element.style['min-height'] = '418px';
-            element.style['display'] = 'flex';
-            element.style['align-items'] = 'center';
+            element.style.minWidth = '800px';
+            element.style.minHeight = '418px';
+            element.style.display = 'flex';
+            element.style.alignItems = 'center';
         }, element);
 
         const boundingBox = await element.boundingBox();
@@ -51,25 +62,30 @@ async function takeScreenshot(query) {
             }
         });
 
-
         if (!fs.existsSync(folder)) {
             fs.mkdirSync(folder, { recursive: true });
         }
 
-        fs.writeFileSync(folder + '/screenshot.png', screenshot);
-	console.log('Screenshot taken for: ' + folder)
+        fs.writeFileSync(screenshotPath, screenshot);
+        console.log('Screenshot taken for: ' + folder);
     } catch (error) {
-	console.log(error)
-   }
-	try {await page.close()} catch(error) {}
-}
+        console.log(error);
+    } finally {
+        try {
+            await page.close();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
 
-const rerun = async function () {
+const rerun = async () => {
+    let browser;
     try {
-        await db.serialize(async () => {
+        db.serialize(async () => {
             try {
                 const rows = await new Promise((resolve, reject) => {
-                    db.all("SELECT * FROM urls limit 1", (err, rows) => {
+                    db.all("SELECT * FROM urls limit 20", (err, rows) => {
                         if (err) {
                             reject(err);
                         } else {
@@ -77,13 +93,16 @@ const rerun = async function () {
                         }
                     });
                 });
-               	if(rows.length === 0) {
-			process.exit(0);
-	         	return;
-		}
 
-		for await (const row of rows) {
-                    await takeScreenshot(row);
+                if (rows.length === 0) {
+                    process.exit(0);
+                    return;
+                }
+
+                browser = await launchBrowser();
+
+                for (const row of rows) {
+                    await takeScreenshot(row, browser);
                     await new Promise((resolve, reject) => {
                         const deleteStmt = db.prepare("DELETE FROM urls WHERE url = ? AND element = ?");
                         deleteStmt.run(row.url, row.element, (err) => {
@@ -97,19 +116,18 @@ const rerun = async function () {
                         });
                     });
                 }
-
+                await browser.close();
             } catch (error) {
-	    console.error(error.message);
+                console.error(error.message);
+                if (browser) {
+                    await browser.close();
+                }
             }
         });
-        setTimeout(rerun, 5000);
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
+        await browser.close();
     }
-}
+};
 
-try {
-setTimeout(rerun, 5000);
-} catch(error) {
-process.exit(0)	
-}
+setTimeout(rerun, 1000);
